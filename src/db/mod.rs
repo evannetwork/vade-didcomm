bcf3a8c834566fe66b9a0a6339bf03727746461e
wulfraem <wulfraem@users.noreply.github.com>
2021-11-24 15:44:42 +0100

    add debug db for inspecting issues with keys
    - enabling feature `debug_db` will use debug db and store all keys to a json text file
    - [DID-610]

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,9 +1,12 @@
-#[cfg(target_arch = "wasm32")]
-mod local_storage;
-#[cfg(not(target_arch = "wasm32"))]
-mod rocks_db;
-
-#[cfg(target_arch = "wasm32")]
-pub(crate) use local_storage::*;
-#[cfg(not(target_arch = "wasm32"))]
-pub(crate) use rocks_db::*;
+cfg_if::cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+        mod local_storage;
+        pub(crate) use local_storage::*;
+    } else if #[cfg(feature = "debug_db")] {
+        mod debug;
+        pub use debug::*;
+    } else {
+        mod rocks_db;
+        pub(crate) use rocks_db::*;
+    }
+}

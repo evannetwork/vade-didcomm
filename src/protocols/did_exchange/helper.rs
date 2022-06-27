04c7d6fbb33102097d509d809c4647f3df353e90
Sebastian Dechant <sdechant@blockchains.com>
2022-06-27 15:14:20 +0200

    allow setting a label for "request" and "response" in did-exchange

--- src/protocols/did_exchange/helper.rs
+++ src/protocols/did_exchange/helper.rs
@@ -133,6 +133,10 @@ pub fn get_did_exchange_message(
                 did_doc_attach: Base64Container {
                     base64: base64_encoded_did_document,
                 },
+                label: match message.base_message.body.get("label") {
+                    Some(label) => serde_json::from_value(label.clone())?,
+                    None => None,
+                },
             }),
             created_time: None,
             expires_time: None,
